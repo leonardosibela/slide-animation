16db79429a3ffdee4cba62f0b2d8829a1efd016f
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-29 15:08:42 -0300

    Implementing click and animation

--- app/src/main/java/com/sibela/slideanimation/MainActivity.kt
+++ app/src/main/java/com/sibela/slideanimation/MainActivity.kt
@@ -2,11 +2,19 @@ package com.sibela.slideanimation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sibela.slideanimation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.root.setOnClickListener {
+            binding.helloWorld.slideUpAlpha(1_000, 0)
+        }
     }
 }
